50296af016089e89aeb72a6b0d1eeb8ee89c5f29
Cleverttech <dzifatedeku@gmail.com>
2021-03-31 07:56:02 +0200

    create iteration 2

--- js/index.js
+++ js/index.js
@@ -6,33 +6,29 @@ function updateSubtotal(product) {
   let price = document.querySelector(".price span");
   let quantity = document.querySelector(".quantity input");
   let subTotal = document.querySelector(".subtotal span");
-  let total = quantity.value * price.innerHTML;
-
-  subTotal.innerText = Number(total);
-  // console.log(subTotal);
+  let total = Number(quantity.value * price.innerHTML);
+  console.log(total);
+  subTotal.innerText = total;
+  return total;
 }
+updateSubtotal();
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  // const singleProduct = document.querySelector(".product");
-  // updateSubtotal(singleProduct);
+  const singleProduct = document.querySelector(".product");
+  updateSubtotal(singleProduct);
 
-  let AllProduct = document.getElementsByClassName("product");
-  let product = "";
-  for (let i = 0; i < AllProduct.length; i++) {
-    product = AllProduct[i].innerHTML;
-    // product = document.querySelector(".product");
-  }
-  updateSubtotal(product);
+  let AllProducts = document.querySelectorAll(".product");
+  let product = 0;
+  AllProducts.forEach((el) => {
+    updateSubtotal(el);
+    product += updateSubtotal(el);
+  });
+  let subTotal1 = document.querySelector("#total span");
+  subTotal1.innerText = product;
+  // updateSubtotal(product);
 
-  // let product = 0;
-  // AllProduct.forEach((el, index) => {
-  //   product += el;
-  //   updateSubtotal(el);
-  // });
-
-  // end of test
   // ITERATION 2
 
   // ITERATION 3
